5210acb12fdf5b230a438d04219033a91704fb41
Adam Rolczyk <adamr6561@gmail.com>
2021-06-13 15:40:06 +0200

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Missing = () => {
+    return (
+        <div>
+            <h1>I created this 404 page, it is not a robot speaking</h1>
+        </div>
+    );
+};
+
+export default Missing;
